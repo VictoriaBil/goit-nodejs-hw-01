d514983dd9578d43a096ce95dd18155dd6161e9e
Victoria Bilauchuk <victoriya.bilauchuk@gmail.com>
2023-04-17 15:45:08 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const { Command } = require("commander");
+const program = new Command();
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+// const yargs = require("yargs");
+// const { hideBin } = require("yargs/helpers");
+
+const contacts = require("./contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.table(allContacts);
+    case "getById":
+      const oneContact = await contacts.getContactById(id);
+      return console.table(oneContact);
+    case "removeById":
+      const removeContact = await contacts.removeContact(id);
+      return console.table(removeContact);
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      return console.table(newContact);
+    default:
+      console.warn("Unknown action type!");
+  }
+};
+
+invokeAction(options);
+
+// const arr = hideBin(process.argv);
+// const { argv } = yargs(arr);
+// invokeAction(argv);
